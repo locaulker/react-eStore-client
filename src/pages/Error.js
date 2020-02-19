2c83b441c11ed0e0544edad77d4846cf540d94ff
Lawrence <Caulker>
2020-02-19 01:27:22 -0500

    setup router for all pages

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Error = () => {
   return (
     <div>
-
+      Oops! 404 Page Not Found
     </div>
   )
 }
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
 
 const ProductDetails = () => {
+  const { id } = useParams()
   return (
     <div>
-
+      Hello from Product Details Page = {id}
     </div>
   )
 }
