f61b290a78cf228f1fd646e8d81e801ca3da44b0
Lawrence <Caulker>
2020-02-20 13:21:50 -0500

    completed project setup

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,14 +1,47 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-
+import { ProductContext } from '../context/productsContext'
+// import { CartContext } from '../context/cartContext'
+import { useHistory } from 'react-router-dom'
+import Loading from '../components/Loading'
 
 const ProductDetails = () => {
   const { id } = useParams()
-  return (
-    <div>
-      Hello from Product Details Page = {id}
-    </div>
-  )
+  const history = useHistory()
+  const { products } = React.useContext(ProductContext)
+
+  const product = products.find(item => item.id === parseInt(id))
+
+  if (products.length === 0) {
+    return <Loading />
+  } else {
+    const {
+      image: { url },
+      title,
+      price,
+      description
+    } = product
+
+    return (
+      <section className="single-product">
+        <img src={url} alt={title} className="single-product-image" />
+        <article>
+          <h1>{title}</h1>
+          <h2>${price}</h2>
+          <p>{description}</p>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              // add to cart
+
+              // where you get directed after adding item to the cart
+              history.push('/cart')
+            }}
+          >Add To Cart</button>
+        </article>
+      </section>
+    )
+  }
 }
 
 export default ProductDetails
